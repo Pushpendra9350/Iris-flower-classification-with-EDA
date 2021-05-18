74d11efedfc85ba4b95f0c44ca249864ced081a8
Pushpendra Kumar <pushpendra9350@gmail.com>
2021-05-18 22:37:30 +0530

    Iris flower

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import joblib
+import flask
+app = Flask(__name__)
+
+# To load a model 
+lr = joblib.load('model.pkl')
+
+# Open when we on the root url
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+# Html page rendered
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+# Will make predictions 
+@app.route('/predict', methods=['POST'])
+def predict():
+    to_predict_list = request.form.to_dict()
+    to_predict_list = list(to_predict_list.values())
+    to_predict_list = np.array(list(map(float, to_predict_list))).reshape(1, -1)
+    print(to_predict_list)
+    prediction = lr.predict(to_predict_list)
+    return jsonify({'prediction': list(prediction)})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
